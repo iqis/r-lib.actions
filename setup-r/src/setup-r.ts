9af7bad20052d796a741c1d20babd9414874fb88
Siqi Zhang <42591382+iqis@users.noreply.github.com>
2023-02-16 14:56:16 -0500

    Update setup-r.ts

--- setup-r/src/setup-r.ts
+++ setup-r/src/setup-r.ts
@@ -5,8 +5,21 @@ import * as path from "path";
 async function run() {
   try {
     core.debug(`started action`);
-    let version = core.getInput("r-version");
-    core.debug(`got version ${version}`);
+   
+    var version: string
+    if (core.getInput("r-version") === "renv") {
+      let renv_lock_path = "./renv.lock";
+      if (fs.existsSync(renv_lock_path)) {
+        let renv_lock = fs.readFileSync(renv_lock_path);
+        version = JSON.parse(renv_lock).R.Version;
+        core.debug(`got version ${version} from renv.lock`);
+      } else {
+        core.setFailed("./renv.lock does not exist.");
+      }
+    } else {
+      version = core.getInput("r-version")
+      core.debug(`got version ${version} from input`);
+    }
 
     await getR(version);
 
